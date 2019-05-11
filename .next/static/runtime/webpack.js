332799f4d5361c4f70c06c1159c7ccc235016f0f
BuildTools <unconfigured@null.spigotmc.org>
2019-05-11 17:13:11 -0400

    what

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d8e89dd35d9f9318fffe";
+/******/ 	var hotCurrentHash = "a44d79d53d503b1108f8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
