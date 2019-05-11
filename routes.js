c1ea82bd742f8591e0eff60ad36eb9ab15e4bf08
BuildTools <unconfigured@null.spigotmc.org>
2019-05-11 16:56:38 -0400

    OH BABY optoins

--- routes.js
+++ routes.js
@@ -2,4 +2,9 @@ const nextRoutes = require('next-routes')
 const routes = module.exports = nextRoutes()
 
 routes.add('index', '/')
-routes.add('thing', '/at/:sll') // ('page file name', 'url to get there/:query')
+// routes.add('thing', '/at/:sll', 'thing') // ('page file name', 'url to get there/:query')
+// this is just another way of doing the above
+routes.add({
+    page: 'thing', // file in pages directory
+    pattern: ['/at/:sll', '/at/:sll/:rere'] // what is typed into url (can be list)
+})
